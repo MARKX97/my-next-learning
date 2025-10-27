3dc227cbce1e3b370e28cd992f3387560beb540e
MARKX97 <MARKX97@users.noreply.github.com>
2025-10-27 16:01:31 +0000

    deploy: edd12b0f5b4412d18a5b01dbe936409a3958ca0e

---
+++ _next/static/chunks/pages/_error-1c00d558581a450c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{7969:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(3127)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=7969)}),_N_E=n.O()}]);
